23768678e75433274cf84a4be2065ff3b1c8f8a0
wong <zhandouxiaojiji@gmail.com>
2019-01-16 01:30:10 +0800

    wtf

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -10,14 +10,52 @@ cc.Class({
         text: 'Hello, World!'
     },
 
+    showHome: function() {
+        console.log("home " + sdkbox.PluginAdMob.isAvailable("home"));
+        sdkbox.PluginAdMob.show("home");
+    },
+    showGameover: function(){
+        console.log("gameover "+sdkbox.PluginAdMob.isAvailable("gameover"));
+        sdkbox.PluginAdMob.show("gameover");
+    },
+
+    admobInit: function() {
+        if(cc.sys.isMobile) {
+            console.log("&&&&&&&&&&");
+            var self = this
+            sdkbox.PluginAdMob.setListener({
+                adViewDidReceiveAd: function(name) {
+                    console.log('adViewDidReceiveAd name=' + name);
+                },
+                adViewDidFailToReceiveAdWithError: function(name, msg) {
+                    console.log('adViewDidFailToReceiveAdWithError name=' + name + ' msg=' + msg);
+                },
+                adViewWillPresentScreen: function(name) {
+                    console.log('adViewWillPresentScreen name=' + name);
+                },
+                adViewDidDismissScreen: function(name) {
+                    console.log('adViewDidDismissScreen name=' + name);
+                },
+                adViewWillDismissScreen: function(name) {
+                    console.log('adViewWillDismissScreen=' + name);
+                },
+                adViewWillLeaveApplication: function(name) {
+                    console.log('adViewWillLeaveApplication=' + name);
+                }
+            });
+            sdkbox.PluginAdMob.init();
+        }
+    },
+
     // use this for initialization
     onLoad: function () {
         this.label.string = this.text;
+        this.admobInit();
         console.log("helloworld");
     },
 
     // called every frame
     update: function (dt) {
-
+        
     },
 });
