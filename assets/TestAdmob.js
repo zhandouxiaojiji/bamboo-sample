5c69b370ac3706be82f6122064d3a45eb0534f0f
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2019-01-16 11:08:27 +0800

    TestAdmob

---
+++ assets/TestAdmob.js
@@ -0,0 +1,87 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    onLoad: function () {
+        //Add this line to onLoad
+        this.admobInit();
+    },
+
+    admobInit: function() {
+        if(cc.sys.isMobile) {
+            var self = this
+            sdkbox.PluginAdMob.setListener({
+                adViewDidReceiveAd: function(name) {
+                    console.log('adViewDidReceiveAd name=' + name);
+                },
+                adViewDidFailToReceiveAdWithError: function(name, msg) {
+                    console.log('adViewDidFailToReceiveAdWithError name=' + name + ' msg=' + msg);
+                },
+                adViewWillPresentScreen: function(name) {
+                    console.log('adViewWillPresentScreen name=' + name);
+                },
+                adViewDidDismissScreen: function(name) {
+                    console.log('adViewDidDismissScreen name=' + name);
+                },
+                adViewWillDismissScreen: function(name) {
+                    console.log('adViewWillDismissScreen=' + name);
+                },
+                adViewWillLeaveApplication: function(name) {
+                    console.log('adViewWillLeaveApplication=' + name);
+                }
+            });
+            sdkbox.PluginAdMob.init();
+        }
+    },
+
+    cacheInterstitial: function() {
+        console.log("cacheInterstitial");
+        if(cc.sys.isMobile) {
+            sdkbox.PluginAdMob.cache('gameover');
+        }
+    },
+
+    showInterstitial: function() {
+        console.log("showInterstitial");
+        if(cc.sys.isMobile) {
+            sdkbox.PluginAdMob.show('gameover');
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
