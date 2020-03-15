b7d314d5e80d8654061d23198971f7a5c26cd2e2
wong@Mac <zhandouxiaojiji@gmail.com>
2020-03-15 10:46:31 +0800

    add prop

---
+++ assets/script/Def/prop.ts
@@ -0,0 +1,8 @@
+//此配置文件由脚本导出，请勿手动修改
+export default {
+	language: {
+		["MENU_CONSOLE"]: {NAME: "MENU_CONSOLE", en: "Console", zh: "控制台", },
+		["MENU_RANK"]: {NAME: "MENU_RANK", en: "Rank", zh: "排行榜", },
+	},
+
+}
