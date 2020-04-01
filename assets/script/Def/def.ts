f25ee3406b923a0eb05ab533ea0aa7f19047778b
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2020-04-01 18:32:12 +0800

    add def

---
+++ assets/script/Def/def.ts
@@ -0,0 +1,51 @@
+import { AdConf } from "../../bamboo/Service/Ad"
+import { WechatAdType } from "../../bamboo/Wechat/WechatAd"
+
+const adConf: AdConf = {}
+
+if (cc.sys.platform == cc.sys.WECHAT_GAME) {
+  adConf.wechat = {
+    bottom: {
+      adName: "bottom",
+      adType: WechatAdType.BANNER,
+      adUnitId: '',
+      style: {
+        bottom: 0,
+        width: 300,
+      }
+    },
+    gameover: {
+      adName: "bottom",
+      adType: WechatAdType.INTERSTITIAL,
+      adUnitId: '',
+    },
+    rewarded: {
+      adName: "bottom",
+      adType: WechatAdType.REWARDED,
+      adUnitId: '',
+    }
+  }
+}
+
+export default {
+  APPNAME: "CHICKEN8",
+  TestAccounts: {
+    "oHuVr5fLlyaIXIqIe2SIvKWiJmgs": true,
+    "ocQYG5lOk7zYKnUZpLzz1day8DcU": true,
+  },
+  TestAccount: "test",
+  HttpHost: {
+    DEV: "http://192.168.5.199:9999",
+    RELEASE: "http://192.168.5.199:8888",
+  },
+  AdName: {
+    BOTTOM: 'bottom',
+    GAMEOVER: 'gameover',
+    REWARDED: 'rewarded',
+  },
+  AdConf: adConf,
+  Wechat: {
+    appId: 'xxx'
+  },
+  
+}
