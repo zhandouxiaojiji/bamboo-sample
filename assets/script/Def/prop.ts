89f453a94bfe3c332657c9849d192a3610b10113
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2020-07-03 17:57:19 +0800

    add resouces/UI

--- assets/script/Def/prop.ts
+++ assets/script/Def/prop.ts
@@ -1,9 +1,9 @@
 //此配置文件由脚本导出，请勿手动修改
 export default {
 	language: {
-		["MENU_CONSOLE"]: {NAME: "MENU_CONSOLE", en: "Console", zh: "控制台", },
-		["MENU_RANK"]: {NAME: "MENU_RANK", en: "Leaderboard", zh: "排行榜", },
-		["MENU_LOGIN"]: {NAME: "MENU_LOGIN", en: "Login", zh: "登录", },
+		["MENU_CONSOLE"]: {NAME: "MENU_CONSOLE", en: "Console", zh: "控制台", aaa: 99999999999, },
+		["MENU_RANK"]: {NAME: "MENU_RANK", en: "Leaderboard", zh: "排行榜", aaa: 0, },
+		["MENU_LOGIN"]: {NAME: "MENU_LOGIN", en: "Login", zh: "登录", aaa: 0, },
 	},
 
 }
